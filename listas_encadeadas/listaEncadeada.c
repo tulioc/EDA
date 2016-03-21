39c951c9841af1f0828501966dd6aaef0a3640a6
Tulio Coelho <tulioopc@gmail.com>
2016-03-21 19:32:16 -0300

    Refatorando Código

--- listas_encadeadas/listaEncadeada.c
+++ listas_encadeadas/listaEncadeada.c
@@ -2,39 +2,6 @@
 Uma lista encadeada, nada mais é, que uma struct com pelo menos duas estruturas:
 Uma dada informação e um ponteiro que aponta para o próximo elemento da lista. Cada posição de uma lista encadeada é chamada de nó.
 
-
-Tipos de listas :
-
- - Lista circular
- Lista que o último elemento aponta para o primeiro elemento, e não para NIL.
- - lista duplamente encadeada
- Lista com apontadores para o próximo elemento, e também para o elemento anterior.
- - Lista circular duplamente encadeada
- - Lista heterogênea
- Lista com elementos heterogêneos.
-
- EX:
-
-typedef struct elemento Elemento;
-
- strcuct elemento {
- int info;
- struct elemento* prox;
-};
-
-Elemento* lst_cria (void) {
-return NULL;
-}
-
-Elemento* lst_insere (Elemento* 1st, int val) {
-
-  Elemento* novo = (Elemento*) malloc(sizeof(Elemento));
-  novo->info = val;
-  novo->prox = 1st;
-  return novo;
-}
-
-
 Funções de remover o elemento e reorganizar...
 * Atualiza o valor da lista, se o elemento removido for o primeiro
 * Caso contrário, apenas remove o elemento da lista.
@@ -44,47 +11,25 @@ Funções de remover o elemento e reorganizar...
 #include <stdio.h>
 #include  <stdlib.h>
 
-typedef struct elemento Elemento;
+typedef struct lista Lista;
 
-struct elemento {
+// Cria a estrutura da lista
+struct lista {
   int valor;
-  Elemento *proximo;
+  Lista *proximo;
 };
 
-Elemento* criaListaNula (void) {
-return NULL;
-}
+// Declarando o 'esqueleto das funções'
 
+Lista* criaListaNula(void);
+Lista* insereValorLista(Lista* lista, int valorRecebido);
+Lista* removerElemento(Lista* lista, int valorRecebido);
+void imprimirLista(Lista* lista);
 
-Elemento* insereValorLista (Elemento* lista, int valorRecebido) {
-
-  Elemento* novo = (Elemento*) malloc(sizeof(Elemento));
-  novo->valor = valorRecebido;
-  novo->proximo = lista;
-
-  return novo;
-}
-
-
-void removerElemento (Elemento *tmp, int valor) {
-
-
-}
-
-void imprimirLista (Elemento *tmp) {
-
-  if(tmp != NULL)
-{
-  printf("%d \n", tmp->valor);
-  imprimirLista(tmp->proximo);
-}
-
-
-}
 
 int main (void) {
 
-  Elemento* lista;
+  Lista* lista;
 
   lista = criaListaNula();
   lista = insereValorLista(lista,10);
@@ -93,6 +38,30 @@ int main (void) {
   lista = insereValorLista(lista,40);
   imprimirLista(lista);
 
-
   return 0;
+}
+
+
+// Cria o primeiro elemento da lista, apontando pra null.
+Lista* criaListaNula(void) {
+  return NULL;
+}
+
+// Insere novo valor em uma dada lista.
+Lista* insereValorLista(Lista* lista, int valorRecebido) {
+
+  Lista* novo = (Lista*) malloc(sizeof(Lista));
+  novo->valor = valorRecebido;
+  novo->proximo = lista;
+  return novo;
+}
+
+// Função para remover elemento de uma dada lista.
+
+void imprimirLista(Lista *lista) {
+
+  if(lista != NULL) {
+  printf("%d \n", lista->valor);
+  imprimirLista(lista->proximo);
+  }
 }
