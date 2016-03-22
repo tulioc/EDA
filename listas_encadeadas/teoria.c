8f83a1e503bc551de97648601266d24f464cee6e
Tulio Coelho <tulioopc@gmail.com>
2016-03-22 12:29:29 -0300

    Outra forma de organizar as estruturas de dados

---
+++ listas_encadeadas/teoria.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+Uma forma melhor de organizar as listas é separar as estruturas como
+Nó = o tipo de dados que você está armazenando
+Lista = sua lista de fato.
+
+typedef struct _no
+{
+
+}No;
+
+typedef struct _lista
+{
+  struct _no * no;
+  struct _lista * proximo;
+}Lista;
+*/
