314cfdf8ac910ca7492189797e50dff7552269c4
Tulio Coelho <tulioopc@gmail.com>
2016-03-22 21:09:53 -0300

    Adicionando comentários no código

--- listas_encadeadas/listaCircular.c
+++ listas_encadeadas/listaCircular.c
@@ -8,11 +8,13 @@ struct lista {
   Lista *proximo;
 };
 
+
+// Declarando esqueleto das funções
 Lista* criaPrimeiroElemento(int valorRecebido);
 Lista* inserirValorListaCircular(Lista* lista, int valorRecebido);
 void imprimirListaCircular(Lista* lista);
 
-
+// Main
 int main (void) {
 
   Lista* lista;
@@ -21,41 +23,47 @@ int main (void) {
   lista = inserirValorListaCircular(lista,15);
   imprimirListaCircular(lista);
   return 0;
+
 }
 
+// =========== Implementação das funções ==============
+
+
+
+// Função que cria o primeiro nó da lista, nó que aponta para NULL.
 Lista* criaPrimeiroElemento(int valorRecebido) {
   Lista* lista;
-  lista = (Lista*) malloc(sizeof(Lista));
-  lista->valor = valorRecebido;
-  lista->proximo = NULL;
-  return lista;
+  lista = (Lista*) malloc(sizeof(Lista)); // Alocando espaço na memória para o primeiro elemento.
+  lista->valor = valorRecebido; // O valor do primeiro elemento será o valor recebido como parâmetro na função.
+  lista->proximo = NULL; // O primeiro elemento irá apontar para NULL.
+  return lista; // A função retorna o ponteiro para o primeiro elemento.
 }
 
+
+// Função que insere valores na lista circular simples
 Lista* inserirValorListaCircular(Lista* lista, int valorRecebido) {
 
+  // Se a função receber uma lista, com um único elemento, ela entra no IF.
   if(lista->proximo == NULL) {
-    Lista* novoElemento = (Lista*) malloc(sizeof(Lista));
-    novoElemento->valor = valorRecebido;
-    novoElemento->proximo = lista;
-    lista->proximo = novoElemento;
-    return novoElemento;
-  } else {
-    Lista* novoElemento = (Lista*) malloc(sizeof(Lista));
-    novoElemento->valor = valorRecebido;
-    novoElemento->proximo = lista;
-    Lista* varredor;
-    varredor = lista;
-    while(varredor->proximo != lista) {
-      varredor = varredor->proximo;
+    Lista* novoElemento = (Lista*) malloc(sizeof(Lista)); // Alocando espaço na memória para o novo elemento da lista.
+    novoElemento->valor = valorRecebido; // O valor do novo elemento da lista irá receber o valor recebido como parâmetro.
+    novoElemento->proximo = lista; // O novo elemento irá apontar para o mesmo elemento que lista aponta.
+    lista->proximo = novoElemento; // O primeiro elemento, que apontava para NULL, agora irá apontar para o novo elemento adicionado, criando assim, a lista circular.
+    return novoElemento; // Retorna o novo elemento como "Cabeça da lista".
+  } else { // Caso o a lista não tenha um único elemento, ela entra no else.
+    Lista* novoElemento = (Lista*) malloc(sizeof(Lista)); // Alocando espaço na memória para o novo elemento da lista.
+    novoElemento->valor = valorRecebido; // O valor do novo elemento da lista irá receber o valor recebido como parâmitro.
+    novoElemento->proximo = lista; // O novo elemento irá apontar para o mesmo elemento que lista aponta.
+    Lista* varredor; // Declarando um ponteiro auxiliar para varrer a lista.
+    varredor = lista; // O ponteiro varredor, inicialmente, irá apontar para o mesmo elemento que lista aponta.
+    while(varredor->proximo != lista) { // Enquanto o varredor não chegar no último elemento
+      varredor = varredor->proximo; //varredor receberá o próximo elemento.
     }
-    varredor->proximo = novoElemento;
-    return novoElemento;
+    varredor->proximo = novoElemento; // Assim que ele encontra o último elemento, ele faz com que o último elemento aponte para o primeiro, dando continuidade a lista circular.
+    return novoElemento; // Retorna o novo elemento como "Cabeça da lista"
   }
-
-
-
 }
-
+// Função que imprime o valor dos elementos da lista.
 void imprimirListaCircular(Lista* lista) {
 
   Lista* varredor;
@@ -64,45 +72,3 @@ void imprimirListaCircular(Lista* lista) {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct no
-{
-  int numeroLegal;
-  no * proximoNo;
-} node;
-
-node * geraNovoNo(int x)
-{
-  node * novoNo = (node*)malloc(sizeof(node));
-  novoNo->numeroLegal = x;
-  novoNo->proximoNo = NULL;
-  return novoNo;
-}
-
-void adicionaNo(node * lista, int x)
-{
-  if(lista == NULL)
-  {
-    lista = gera
-  }
-}
-*/
