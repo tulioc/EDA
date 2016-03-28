947fc04a019eb0ff6925a83ccf00893b944271c4
Tulio Coelho <tulioopc@gmail.com>
2016-03-27 22:13:50 -0300

    Adicionando funções de remover elemento e de imprimir lista usando recursividade

--- recursividade/listasEncadeadas.c
+++ recursividade/listasEncadeadas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Lista lista;
+typedef struct lista Lista;
 
 struct lista {
 int valor;
@@ -9,15 +9,60 @@ Lista* proximo;
 };
 
 Lista* criaLista(int valorRecebido);
+Lista* insereElemento(Lista* lista, int valorRecebido);
+Lista* deletaElementoRec(Lista* lista, int valorRecebido);
+void imprimeLista(Lista* lista);
 
 
 int main (void) {
 
+  Lista* exemplo;
+  exemplo = criaLista(70);
+  exemplo = insereElemento(exemplo,15);
+  exemplo = insereElemento(exemplo,20);
+  imprimeLista(exemplo);
+  printf("\n");
+  exemplo = deletaElementoRec(exemplo,15);
+  imprimeLista(exemplo);
 
 
   return 0;
 }
 
 Lista* criaLista(int valorRecebido) {
-  
+  Lista* primeiroElemento;
+  primeiroElemento = (Lista*) malloc(sizeof(Lista));
+  primeiroElemento->valor = valorRecebido;
+  primeiroElemento->proximo = NULL;
+  return primeiroElemento;
+}
+
+Lista* insereElemento(Lista* lista, int valorRecebido) {
+  Lista* novoElemento;
+  novoElemento = (Lista*) malloc(sizeof(Lista));
+  novoElemento->valor = valorRecebido;
+  novoElemento->proximo = lista;
+  return novoElemento;
+}
+
+Lista* deletaElementoRec(Lista* lista, int valorRecebido) {
+  if (lista != NULL) {
+
+    if(lista->valor == valorRecebido) {
+      Lista* aux = lista;
+      lista = lista->proximo;
+      free(aux);
+    }
+    else {
+      lista->proximo = deletaElementoRec(lista->proximo, valorRecebido);
+    }
+  }
+  return lista;
+}
+
+void imprimeLista (Lista* lista) {
+  if(lista != NULL) {
+  printf("%d \n", lista->valor);
+  imprimeLista(lista->proximo);
+  }
 }
