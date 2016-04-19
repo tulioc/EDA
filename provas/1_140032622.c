592579d34095bab5c436299f361198db32bc9507
Tulio Coelho <tulioopc@gmail.com>
2016-04-19 16:09:29 -0300

    Reorganizando estrutura de pastas

--- provas/1_140032622.c
+++ provas/1_140032622.c
@@ -26,6 +26,54 @@ void imprimeLista(Gestor *lista);
 
 int main (void) {
 
+  int contador = 0;
+  int quantidade_max = 0;
+  int numero_processos = 0;
+  int id = 0;
+  int registros_adicionais = 0;
+
+
+  scanf("%d",&quantidade_max);
+  //printf("Quantidade maxima : %d\n", quantidade_max);
+  scanf("%d",&numero_processos);
+// printf("Quantidade de processos : %d\n", numero_processos);
+
+  Gestor* gestor = criaGestor(quantidade_max);
+  for(; contador < numero_processos; contador++) {
+    scanf("%d",&id);
+    //printf("ID a ser adicionado : %d\n", id);
+    insereProcesso(gestor,id);
+  }
+
+  contador = 0;
+  id = 0;
+
+  scanf("%d",&registros_adicionais);
+  //printf("R : %d\n", registros_adicionais);
+  char tipo_registro;
+
+  for(; contador < registros_adicionais; contador++) {
+    scanf("%d",&id);
+    //printf("ID a ser adicionado : %d\n", id);
+    scanf(" %c",&tipo_registro);
+    //printf("Tipo de registro  : %c\n",tipo_registro);
+    if (tipo_registro == 't') {
+      if(gestor->quantidade_nos > 0) {
+          removeID(gestor,id);
+      }
+    }
+    else if (tipo_registro == 'n') {
+      if(gestor->quantidade_nos < quantidade_max) {
+          insereProcesso(gestor,id);
+      }
+
+    } else {
+      // NADA
+    }
+  }
+
+  imprimeLista(gestor);
+
   return 0;
 }
 
@@ -57,14 +105,14 @@ void insereProcesso(Gestor* gestor, int valor_id) {
     gestor->fim = novo_elemento;
     gestor->quantidade_nos++;
 
-  } else if (gestor->quantidade_nos > 0 && gestor->quantidade_nos <= gestor->quantidade_max) {
+  } else if (gestor->quantidade_nos > 0 && gestor->quantidade_nos < gestor->quantidade_max) {
     novo_elemento->anterior = gestor->fim;
     gestor->fim->proximo = novo_elemento;
     gestor->fim = novo_elemento;
     gestor->quantidade_nos++;
 
   } else {
-    printf("Limite atingido \n");
+
   }
 }
 
@@ -84,34 +132,52 @@ Node* buscaID(Gestor* gestor, int alvo) {
 
 void removeID(Gestor* gestor, int alvo) {
 
-  Node* no_alvo = buscaID(gestor, alvo);
 
-  if(no_alvo != NULL) {
-    if(no_alvo == gestor->fim) {
-      gestor->fim = no_alvo->anterior;
-      gestor->fim->proximo = NULL;
-      gestor->quantidade_nos--;
-      free(no_alvo);
 
+  if (gestor->quantidade_nos > 0) {
+    Node* no_alvo = buscaID(gestor, alvo);
+    if(no_alvo != NULL) {
+      if(no_alvo == gestor->fim) {
+        gestor->fim = no_alvo->anterior;
+        gestor->fim->proximo = NULL;
+        gestor->quantidade_nos--;
+        free(no_alvo);
+
+      } else {
+        Node* kill;
+        int salvaID = gestor->fim->id;
+        gestor->fim->id = no_alvo->id;
+        no_alvo->id = salvaID;
+        kill = gestor->fim;
+        gestor->fim = gestor->fim->anterior;
+        gestor->fim->proximo = NULL;
+        gestor->quantidade_nos--;
+        free(kill);
+      }
     } else {
-      int salvaID = gestor->fim->id;
-      gestor->fim->id = no_alvo->id;
-      no_alvo->id = salvaID;
-      gestor->fim = no_alvo;
-      gestor->fim->proximo = NULL;
-      gestor->quantidade_nos--;
-      free(no_alvo->proximo);
+      printf("ID não encontrado\n");
     }
   } else {
-    printf("ID não encontrado\n");
+
   }
 }
 
 void imprimeLista(Gestor* gestor) {
   int contador = 0;
   Node* auxiliar = gestor->inicio;
-  for(; contador < gestor->quantidade_nos; contador++) {
-    printf("%d", auxiliar->id);
-    auxiliar = auxiliar->proximo;
+  if (gestor->quantidade_nos == 0) {
+      printf("V = []\n");
+  } else {
+    printf("V = [");
+    for (; contador < gestor->quantidade_nos; contador++) {
+
+      if (contador == (gestor->quantidade_nos - 1)) {
+        printf("%d", auxiliar->id);
+      } else {
+        printf("%d, ", auxiliar->id);
+      }
+      auxiliar = auxiliar->proximo;
+    }
+    printf("]\n");
   }
 }
